e55ac4f9bc4c556ddac34aa63a04f59e0da414b7
Kalash Patil <102899864+kalashpatil33@users.noreply.github.com>
2023-07-08 08:49:50 +0530

    Added solution - LeetHub

--- Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
@@ -9,53 +9,7 @@ using namespace std;
 *  arr[]: input array
 *  n: size of array
 */
-class Solution{
-  public:
-    //Function to find triplets with zero sum.
-    bool findTriplets(int arr[], int n)
-    { 
-         if(n==3)
-    {
-        if(arr[1]+arr[2]+arr[0]==0)
-        return true;
-        else
-        return false;
-    }
-    else
-    {
-    sort(arr,arr+n);
-    int f=0;
-   
-      for(int i=0;i<n;i++)
-      {
-          while(i!=0&&i<n&&arr[i]==arr[i-1])
-          {
-              if(arr[i]!=0)
-              f=1;
-              i++;
-          }
-          
-          int target=arr[i];
-          int st=i+1;
-          int end=n-1;
-          while(st<end)
-          {
-              if((arr[st]+arr[end]+target)==0)
-              {
-                  return true;
-              }
-              if((arr[st]+arr[end]+target)>0)
-              {
-                  end--;
-              }
-              else
-              st++;
-          }
-      }
-    }
-      return false;
-    }
-};
+
 
 //{ Driver Code Starts.
 int main()
