11ceec6691ccb8ec1eb8ef8ff3a00f4813e4452d
Kalash Patil <102899864+kalashpatil33@users.noreply.github.com>
2023-07-10 10:14:38 +0530

    Added solution - LeetHub

---
+++ Pattern Jumping - GFG/pattern-jumping.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string canJump(long long N) {
+        return (N&(N-1))==0?"True":"False";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.canJump(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
