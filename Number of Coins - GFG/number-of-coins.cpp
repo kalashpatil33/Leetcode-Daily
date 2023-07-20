16c8a90ba68aa31145e99c5a75416abd99944970
Kalash Patil <102899864+kalashpatil33@users.noreply.github.com>
2023-07-20 08:45:51 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -6,32 +6,38 @@ using namespace std;
 class Solution{
 
 	public:
-	int solve(int ind,int nums[],int amt, vector<vector<int>> &dp)
-	{
-	    if(ind==0)
-	    {
-	        if(amt%nums[0]==0)
-	        return dp[ind][amt]=amt/nums[0];
-	        else
-	        return dp[ind][amt]=1e9;
-	    }
-	    if(dp[ind][amt]!=-1)
-	    {
-	        return dp[ind][amt];
-	    }
-	    int pick=1e9,notpick=1e9;
-	    
-	    if(nums[ind]<=amt)
-	    pick=1+solve(ind,nums,amt-nums[ind],dp);
-	    
-	    notpick=0+solve(ind-1,nums,amt,dp);
-	    
-	    return dp[ind][amt]=min(pick,notpick);
-	}
 	int minCoins(int coins[], int M, int V) 
 	{ 
-	    vector<vector<int>> dp(M,vector<int> (V+1,-1));
-	   return (solve(M-1,coins,V,dp)==1e9)?-1:solve(M-1,coins,V,dp);
+	    vector<vector<int>> dp(M,vector<int> (V+1,1e9));
+	    
+	    for(int i=0;i<=V;i++)
+	    {
+	        if(i%coins[0]==0)
+	        {
+	            dp[0][i]=i/coins[0];
+	        }
+	    }
+	    
+	    for(int i=1;i<M;i++)
+	    {
+	        for(int j=0;j<=V;j++)
+	        {
+	   int pick=1e9,notpick=1e9;
+	    
+	    if(coins[i]<=j)
+	    pick=1 + dp[i][j-coins[i]];
+	    
+	    notpick=0+dp[i-1][j];
+	    
+	    dp[i][j]=min(pick,notpick);
+	        }
+	    }
+	    
+	    if(dp[M-1][V]==1e9)
+	    return -1;
+	    else
+	    return dp[M-1][V];
+    
 	} 
 	  
 };
