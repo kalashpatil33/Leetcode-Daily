bff82d918f6ba88fd8750d0c426570ebf51787be
Kalash Patil <102899864+kalashpatil33@users.noreply.github.com>
2023-09-30 17:23:02 +0530

    Added solution - LeetHub

--- Search in a Rotated Array - GFG/search-in-a-rotated-array.cpp
+++ Search in a Rotated Array - GFG/search-in-a-rotated-array.cpp
@@ -5,43 +5,41 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int search(int A[], int l, int h, int key){
+    int search(int x[], int l, int h, int key){
         // l: The starting index
         // h: The ending index, you have to search the key in this range
         
         //complete the function here
-        
-        
         while(l<=h)
         {
-            int m=(l+h)/2;
-            
-            
-            if(A[m]==key)
-            return m;
-            if(A[m]>=A[l])
+            int mid=(l+h)/2;
+            if(x[mid]==key)
+            return mid;
+            if(x[l]<=x[mid])              //array is sorted in left half
             {
-              if(key>=A[l]&&key<=A[m])
-              {
-                  h=m-1;
-              }
-              else
-              l=m+1;
-            }
-            else{
-                if(key<=A[h]&&key>=A[m])
+                if(x[l]<=key && key<=x[mid])
                 {
-                    l=m+1;
+                    h=mid-1;
                 }
                 else{
-                    h=m-1;
+                    l=mid+1;
                 }
             }
+            else{                //sorted right half
+                    if(x[mid]<=key && key<=x[h])
+                    {
+                        l=mid+1;
+                    }
+                    else{
+                        h=mid-1;
+                    }
+                }
         }
-        
         return -1;
+        
     }
 };
+
 
 //{ Driver Code Starts. 
 int main() {
