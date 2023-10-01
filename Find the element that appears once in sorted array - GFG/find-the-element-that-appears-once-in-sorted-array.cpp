d9f4996f612bf27770194057507a0004b932f0c8
Kalash Patil <102899864+kalashpatil33@users.noreply.github.com>
2023-10-01 07:13:13 +0530

    Added solution - LeetHub

--- Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -9,14 +9,33 @@ using namespace std;
 class Solution
 {
   public:
-    int findOnce(int x[], int n)
+    int findOnce(int arr[], int n)
     {
         //code here.
-        int xr=0;
-        for(int i=0;i<n;i++)
-        xr= xr xor x[i];
+        if(n==1)
+        return arr[0];
         
-        return xr;
+        if(arr[0]!=arr[1])
+        return arr[0];
+        
+        if(arr[n-1]!=arr[n-2])
+        return arr[n-1];
+
+        int l=1,h=n-2;             //eliminating the extra conditions we handled them additionally above
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(arr[mid]!=arr[mid-1] and arr[mid]!=arr[mid+1])
+            return arr[mid];
+            if((mid%2==0 and arr[mid]==arr[mid+1]) or (mid%2==1 and arr[mid]==arr[mid-1])) //odd eevn pattern
+            {
+                l=mid+1;              //element is in right half
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return -1;
     }
 };
 
